8e777521104d53ea41d6bcfb282b6dd33794cf4a
Jennifer Averett <Jennifer.Averett@OARcorp.com>
2010-05-19 15:03:43 +0000

    2010-05-19	Jennifer Averett <Jennifer.Averett@OARcorp.com>
    	* Target_powerpc.cc: Added missing branch instructions to table of
    	branch instructions for powerpc.

--- rtems-coverage/Target_powerpc.cc
+++ rtems-coverage/Target_powerpc.cc
@@ -19,12 +19,19 @@ namespace Target {
   Target_powerpc::Target_powerpc( std::string targetName ):
     TargetBase( targetName )
   {
+    branchInstructions.push_back("b");
+    branchInstructions.push_back("ba");
+    branchInstructions.push_back("bl");
+    branchInstructions.push_back("bla");
     branchInstructions.push_back("bc");
     branchInstructions.push_back("bca");
     branchInstructions.push_back("bcl");
     branchInstructions.push_back("bcla");
     branchInstructions.push_back("bcctr");
     branchInstructions.push_back("bcctrl");
+    branchInstructions.push_back("bclr");
+    branchInstructions.push_back("bclrl");
+
   
     branchInstructions.sort();    
   }
