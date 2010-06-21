cc67fa7cac07b90f3ee3159001f5a831f592cd86
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-21 16:51:16 +0000

    2010-06-21	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* tmtest/Makefile.am, tmtest/TEST.doc, tmtest/init.c: New files.

---
+++ rtems-test-template/tmtest/init.c
@@ -0,0 +1,41 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#include <coverhd.h>
+#include <tmacros.h>
+#include <timesys.h>
+#include "test_support.h"
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  puts( "\n\n*** TIME TEST @DESC@ ***" );
+
+  /* XXX test code goes here */
+  
+  puts( "*** END OF TIME TEST @DESC@ ***" );
+
+  rtems_test_exit(0);
+}
+
+/* configuration information */
+
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
+
+#define CONFIGURE_MAXIMUM_TASKS             1
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT
+
+#include <rtems/confdefs.h>
+/* end of file */
