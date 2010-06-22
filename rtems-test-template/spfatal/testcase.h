9628a69d5fa69de2cdd0e61a2945e7e35cd26046
Joel Sherrill <joel.sherrill@OARcorp.com>
2010-06-22 01:27:20 +0000

    2010-06-21	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* spfatal/Makefile.am, spfatal/TEST.doc, spfatal/TEST.scn,
    	spfatal/testcase.h: New files.

---
+++ rtems-test-template/spfatal/testcase.h
@@ -0,0 +1,25 @@
+/*
+ *  COPYRIGHT (c) 1989-2010.
+ *  On-Line Applications Research Corporation (OAR).
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+/* XXX these will have to be tailored */
+#define FATAL_ERROR_TEST_NAME            "@DESC@"
+#define FATAL_ERROR_DESCRIPTION          "@DESC@"
+#define FATAL_ERROR_EXPECTED_SOURCE      INTERNAL_ERROR_RTEMS_API
+#define FATAL_ERROR_EXPECTED_IS_INTERNAL FALSE
+#define FATAL_ERROR_EXPECTED_ERROR       0
+
+void force_error()
+{
+  /* XXX if not triggered directly or indirectly via a configuration */
+  /* XXX error, then do something here to trigger it */
+
+  /* we will not run this far */
+}
