0258ba5b965cda6fdfff8d22cba19e2dac670926
Joel Sherrill <joel.sherrill@OARcorp.com>
2011-06-10 13:19:07 +0000

    2011-06-10	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* psxtmtest_single/init.c: More meat.

--- rtems-test-template/psxtmtest_single/init.c
+++ rtems-test-template/psxtmtest_single/init.c
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2010.
+ *  COPYRIGHT (c) 1989-2011.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -18,10 +18,26 @@ void *POSIX_Init(
   void *argument
 )
 {
+  long end_time;
+
   puts( "\n\n*** POSIX TIME TEST @DESC@ ***" );
 
-  /* XXX test code goes here */
-  
+  /* XXX any required initialization goes here */
+
+  benchmark_timer_initialize();
+
+  /* XXX single shot operation goes here */
+
+  end_time = benchmark_timer_read();
+
+  put_time(
+    "@DESC@",
+    end_time,
+    OPERATION_COUNT,
+    0,
+    0
+  );
+
   puts( "*** END OF POSIX TIME TEST @DESC@ ***" );
 
   rtems_test_exit(0);
@@ -32,7 +48,7 @@ void *POSIX_Init(
 #define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
 #define CONFIGURE_APPLICATION_NEEDS_TIMER_DRIVER
 
-#define CONFIGURE_MAXIMUM_POSIX_THREADS     2
+#define CONFIGURE_MAXIMUM_POSIX_THREADS     1
 #define CONFIGURE_POSIX_INIT_THREAD_TABLE
 
 #define CONFIGURE_INIT
