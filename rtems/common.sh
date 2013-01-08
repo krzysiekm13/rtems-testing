30a531687b01b0c8abd2045286b91af3024ea7dd
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-01-08 14:37:49 -0600

    bit_all_bsps: Add support for GNU parallel driven builds
    This was a significant rework which added command line arguments
    and a dependency on GNU parallel if the -p option is used. This allows
    a build machine to be fully utilized while building all BSPs in a
    particular configuration.

--- rtems/common.sh
+++ rtems/common.sh
@@ -66,7 +66,15 @@ target=${CPU}-rtems
 
 case ${CPU} in
   a29k)        ;;
-  arm)         ;;
+  #arm)         ;;
+  arm)
+    # Cover up some insanity
+    CPU=arm
+    type arm-rtemseabi4.11-gcc >/dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+      target=arm-rtemseabi4.11
+    fi
+    ;;
   avr)         ;;
   bfin)         ;;
   c3x)         CPU=c4x ; target=c4x-rtems ;;
