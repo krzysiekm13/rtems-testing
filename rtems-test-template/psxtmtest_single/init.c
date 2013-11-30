82cb15c5308c717cc405f0c3f03e57a9e331d75e
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-11-30 09:12:06 -0600

    test templates: Update copyright year

--- rtems-test-template/psxtmtest_single/init.c
+++ rtems-test-template/psxtmtest_single/init.c
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2012.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
--- rtems-test-template/spfatal/testcase.h
+++ rtems-test-template/spfatal/testcase.h
@@ -1,5 +1,5 @@
 /*
- *  COPYRIGHT (c) 1989-2011.
+ *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
